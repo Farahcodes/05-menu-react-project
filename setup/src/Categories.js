cf8ac6b1eb7b7b2230f2ce6b8be4f3ae4c980651
farah <farah.fournat@gmail.com>
2022-01-01 20:54:23 +0100

    added all button

--- setup/src/Categories.js
+++ setup/src/Categories.js
@@ -4,6 +4,7 @@ const Categories = ({filterItems}) => {
   return(
     <div className='btn-container'>
       <button className='filter-btn' onClick={()=> filterItems('breakfast')}>breakfast</button>
+      <button className='filter-btn' onClick={()=> filterItems('all')}>all</button>
     </div>
   );
 };
